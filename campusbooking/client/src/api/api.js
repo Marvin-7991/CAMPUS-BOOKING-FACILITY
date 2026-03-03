cdeb96192371dfdececa4c72c6d932516522c2ea
Marvin Asare <155252319+Marvin-7991@users.noreply.github.com>
2026-03-03 11:21:27 +0000

    Change API base URL to production server

--- campusbooking/client/src/api/api.js
+++ campusbooking/client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://campus-booking-facility-ffnp.onrender.com',
 });
 
 // Attach JWT token to every request if present
